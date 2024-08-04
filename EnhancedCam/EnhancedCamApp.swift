07cdb54ddc27e217f08be8e2da68eba03df5cb50
Andriyanto Halim <andriyanto.halim@gmail.com>
2024-08-04 17:39:48 +0800

    first commit

--- EnhancedCam/EnhancedCamApp.swift
+++ EnhancedCam/EnhancedCamApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct EnhancedCamApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            EnhancedCamView()
         }
     }
 }
---
+++ EnhancedCamView.swift
@@ -0,0 +1,56 @@
+//
+//  EnhancedCamView.swift
+//  EnhancedCam
+//
+//  Created by Andriyanto Halim on 4/8/24.
+//
+
+import SwiftUI
+import AVFoundation
+
+struct EnhancedCamView: View {
+    @ObservedObject var viewModel = EnhancedCamViewModel()
+
+    var body: some View {
+        ZStack {
+            CameraPreview(previewLayer: viewModel.getPreviewLayer())
+                .edgesIgnoringSafeArea(.all)
+
+            VStack {
+                Spacer()
+                Button(action: {
+                    viewModel.capturePhoto()
+                }) {
+                    Circle()
+                        .fill(Color.white)
+                        .frame(width: 70, height: 70)
+                        .overlay(
+                            Circle()
+                                .stroke(Color.gray, lineWidth: 2)
+                                .frame(width: 60, height: 60)
+                        )
+                }
+                .padding(.bottom, 30)
+            }
+        }
+    }
+}
+
+struct CameraPreview: UIViewRepresentable {
+    let previewLayer: AVCaptureVideoPreviewLayer?
+
+    func makeUIView(context: Context) -> UIView {
+        let view = UIView()
+        if let previewLayer = previewLayer {
+            previewLayer.frame = UIScreen.main.bounds
+            view.layer.addSublayer(previewLayer)
+        }
+        return view
+    }
+
+    func updateUIView(_ uiView: UIView, context: Context) {}
+}
+
+#Preview {
+    EnhancedCamView()
+}
