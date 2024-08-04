3e080bc4fc68c62d5a6d7de08138a0a542ebd200
Andriyanto Halim <andriyanto.halim@gmail.com>
2024-08-04 17:28:20 +0800

    Initial Commit

---
+++ EnhancedCam/EnhancedCamApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnhancedCamApp.swift
+//  EnhancedCam
+//
+//  Created by Andriyanto Halim on 4/8/24.
+//
+
+import SwiftUI
+
+@main
+struct EnhancedCamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
